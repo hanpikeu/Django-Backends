94755f51801746f75a05f36c191bea9e66f1f52d
윤재상 <apple01644@gmail.com>
2020-03-07 23:24:53 +0900

    Add discord_bot

--- chinagate/wsgi.py
+++ chinagate/wsgi.py
@@ -1,7 +1,8 @@
 import os
-
+import subprocess
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinagate.settings')
 
+subprocess.run('python discord_bot.py')
 application = get_wsgi_application()
---
+++ discord_bot.py
@@ -0,0 +1,20 @@
+import os
+import discord
+import discord.utils
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(msg: discord.Message):
+    if msg.channel.id == 683302727912390770:
+        if msg.content in ['!로그인', '!로긴']:
+            print(msg.author)
+            await msg.author.send("이 글이 보이시면 말 해주세요.")
+
+client.run(os.getenv('BOT_TOKEN'))
