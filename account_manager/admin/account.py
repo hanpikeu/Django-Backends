d23f9b7771bb5afed4dea68023846535e2528f8f
윤재상 <banana2692@daum.net>
2020-03-10 15:35:06 +0900

    Feature. Account Manager Admin site
    Account.discord_id is uneditable

--- account_manager/admin/account.py
+++ account_manager/admin/account.py
@@ -5,8 +5,24 @@ from account_manager.models.account import Account
 
 @admin.register(Account)
 class AdminInterfaceAccount(admin.ModelAdmin):
-    readonly_fields = [
-        'account_id',
-        'created_time',
-        'modified_time',
-    ]
+    readonly_fields = ['account_id', 'created_time', 'modified_time', 'token', 'token_create_time']
+
+    list_display = ['account_id', 'discord_id', 'is_able', 'token_create_time']
+
+    fieldsets = (
+        ('Main', {
+            'fields': ('account_id', 'is_able', 'discord_id')
+        }),
+        ('Log', {
+            'fields': ('created_time', 'modified_time')
+        }),
+        ('Token', {
+            'fields': ('token', 'token_create_time')
+        }),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        read_only_fields = ['account_id', 'created_time', 'modified_time', 'token', 'token_create_time']
+        if obj is not None:
+            read_only_fields.append('discord_id')
+        return read_only_fields
