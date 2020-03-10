b2620bed6a1e93b216ab77d212a1fed6c510a5aa
윤재상 <banana2692@daum.net>
2020-03-10 18:39:01 +0900

    Update. Assign url for each project

--- chinagate/urls.py
+++ chinagate/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('account_manager.urls')),
+    path('account_manager/', include('account_manager.urls')),
+    path('covidic/', include('covidic.urls')),
+    path('notik/', include('notik.urls')),
 ]
