888928a9239d5284265bc0ba1edf7bf63481a92d
윤재상 <banana2692@daum.net>
2020-03-13 18:38:32 +0900

    Fix mistyping

--- chinagate/urls.py
+++ chinagate/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from update_manager import webhook
 
 urlpatterns = [
-    path('webhook/', webhook),
+    path('webhook', webhook),
     path('admin/', admin.site.urls),
     path('account_manager/', include('account_manager.urls')),
     path('covidic/', include('covidic.urls')),
