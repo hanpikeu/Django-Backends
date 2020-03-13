336b131423d801c6823c2315c8ae44d0a7770798
윤재상 <banana2692@daum.net>
2020-03-13 16:15:48 +0900

    Add name filed to covidic.tag_of_archive Model

--- covidic/admin/tag_of_archive.py
+++ covidic/admin/tag_of_archive.py
@@ -6,6 +6,7 @@ from covidic.models.tag_of_archive import TagOfArchive
 @admin.register(TagOfArchive)
 class AdminInterfaceTagOfArchive(admin.ModelAdmin):
     list_display = ['tag_of_archive_id', 'name']
+    fields = ['name']
 
     def get_readonly_fields(self, request, obj=None):
         read_only_fields = []
