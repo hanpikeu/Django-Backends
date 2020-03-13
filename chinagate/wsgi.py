15eb0cb0379f2c858e9fee70b0ab7cad72499c42
윤재상 <banana2692@daum.net>
2020-03-13 18:54:32 +0900

    Discard Discord.bot.py

--- chinagate/wsgi.py
+++ chinagate/wsgi.py
@@ -1,12 +1,6 @@
 import os
-import subprocess
-
-import psutil
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinagate.settings')
 
 application = get_wsgi_application()
-
-if len(psutil.Process().children()) == 0:
-    subprocess.Popen('python3 discord_bot.py', shell=True)
