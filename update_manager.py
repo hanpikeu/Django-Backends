61c69fabf17bc61daaaf54b8352a9b87ca96f5b5
윤재상 <banana2692@daum.net>
2020-03-13 23:00:53 +0900

    Feature. Update_manager verify webhook_secret

--- update_manager.py
+++ update_manager.py
@@ -1,4 +1,6 @@
+import hmac
 import json
+import os
 import subprocess
 
 from django.http import HttpRequest, HttpResponse
@@ -11,6 +13,21 @@ def webhook(req: HttpRequest):
     if req.method != 'POST':
         return HttpResponse(status=405)
 
+    signature = req.GET['X-Hub-Signature']
+
+    if signature is None:
+        return HttpResponse(status=403)
+
+    sha_name, signature = signature.split('=')
+
+    if sha_name != 'sha1':
+        return HttpResponse(status=501)
+
+    mac = hmac.new(str(os.getenv('WEBHOOK_SECRET')), msg=req.body, digestmod='sha1')
+
+    if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
+        return HttpResponse(status=403)
+
     try:
         data = json.loads(req.body.decode('utf-8'))
 
@@ -22,7 +39,7 @@ def webhook(req: HttpRequest):
 
         if data["pusher"]["email"] != "apple01644@gmail.com":
             return HttpResponse(status=200)
-        
+
         subprocess.Popen('sudo bash update.sh', shell=True)
         return HttpResponse(status=202)
     except Exception:
