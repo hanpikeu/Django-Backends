c01c62870965eb2466907b42cb6e79d5ea5fcc3f
윤재상 <banana2692@daum.net>
2020-03-13 18:54:51 +0900

    Feature. Update Manager

--- update_manager.py
+++ update_manager.py
@@ -1,8 +1,29 @@
-from django.http import HttpResponse
+import json
+import subprocess
+
+from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-def webhook(req):
+def webhook(req: HttpRequest):
     print(req)
-    return HttpResponse(status=200)
+    if req.method != 'POST':
+        return HttpResponse(status=405)
+
+    try:
+        data = json.loads(req.body.decode('utf-8'))
+
+        if data["ref"] != "refs/heads/master":
+            return HttpResponse(status=200)
+
+        if data["repository"]["html_url"] != "https://github.com/China-Gate/Django-Backends":
+            return HttpResponse(status=200)
+
+        if data["pusher"]["email"] != "apple01644@gmail.com":
+            return HttpResponse(status=200)
+        
+        subprocess.Popen('sudo bash update.sh', shell=True)
+        return HttpResponse(status=202)
+    except Exception:
+        return HttpResponse(status=500)
