68b7246570f094637bab704a947f036978630a3f
윤재상 <banana2692@daum.net>
2020-03-13 18:37:25 +0900

    Add Update Manager

--- chinagate/urls.py
+++ chinagate/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from update_manager import webhook
 
 urlpatterns = [
+    path('webhook/', webhook),
     path('admin/', admin.site.urls),
     path('account_manager/', include('account_manager.urls')),
     path('covidic/', include('covidic.urls')),
