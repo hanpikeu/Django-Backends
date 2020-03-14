de239b489a5ebfd83a2600a3e0b4db21ec928a37
윤재상 <banana2692@daum.net>
2020-03-14 12:16:30 +0900

    Debug. Test

--- service_manager/update_repo.sh
+++ service_manager/update_repo.sh
@@ -8,11 +8,13 @@ for argument in "${arguments[@]}"
 do
   case "${argument}" in
     "-m")
+    echo `pwd`
     bash stop_django.sh
     bash migrate_django.sh
     bash stop_django.sh
     ;;
     "-d")
+    echo `pwd`
     bash stop_discord_bot.sh
     bash start_discord_bot.sh
     ;;
