1b8ae0c327918b1f93fd411efff09e88a3330db6
윤재상 <banana2692@daum.net>
2020-03-15 21:25:59 +0900

    Refac. Discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -58,7 +58,7 @@ class HotPostCrawler:
             for pair in HotPostCrawler.load():
                 if not (pair['link'] in self.staged_link):
                     try:
-                        self.channel.send(f'>>> {pair["title"]}\n {pair["link"]}')
+                        await self.channel.send(f'>>> {pair["title"]}\n {pair["link"]}')
                         print(f'>>> {pair["title"]}\n {pair["link"]}')
                         self.staged_link.append(pair['link'])
                     except Exception as e:
@@ -84,48 +84,48 @@ async def on_message(msg: discord.Message):
             res = requests.get(f'http://127.0.0.1:8000/set_token?discord_id={msg.author.id}')
             if res.status_code == 200:
                 load = eval(res.content.decode('utf-8'))
-                await msg.author.send(load['link'])
+                await msg.channel.send(load['link'])
             elif res.status_code == 403:
-                await msg.author.send('권한이 부여되지 않았습니다. 개발팀에게 문의해주시길 바랍니다.')
+                await msg.channel.send('권한이 부여되지 않았습니다. 개발팀에게 문의해주시길 바랍니다.')
             elif res.status_code == 404:
-                await msg.author.send('목록에 없는 유저입니다. 개발팀에게 문의해주시길 바랍니다.')
+                await msg.channel.send('목록에 없는 유저입니다. 개발팀에게 문의해주시길 바랍니다.')
         elif msg.author.id == 348066198983933954:
             if msg.content == '!념글크롤':
                 await msg.author.send('>>> 6초 단위로 념글의 상태변화를 알려주는 명령어 입니다.\n!념글크롤 스테이지\n!념글크롤 시작\n!념글크롤 정지\n!념글크롤 상태')
             elif msg.content == '!념글크롤 스테이지':
                 try:
                     hot_post_crawler.stage()
-                    await msg.author.send('성공적으로 스테이지했습니다.')
+                    await msg.channel.send('성공적으로 스테이지했습니다.')
                 except Exception as e:
                     text = '스테이지중 에러 발생 ' + str(e)
-                    await msg.author.send(text)
+                    await msg.channel.send(text)
             elif msg.content == '!념글크롤 시작':
                 if hot_post_crawler.run:
-                    await msg.author.send('이미 동작하고 있습니다.')
+                    await msg.channel.send('이미 동작하고 있습니다.')
                 else:
                     try:
                         hot_post_crawler.start()
-                        await msg.author.send('성공적으로 시작했습니다.')
+                        await msg.channel.send('성공적으로 시작했습니다.')
                     except Exception as e:
                         text = '시작중 에러 발생 ' + str(e)
-                        await msg.author.send(text)
+                        await msg.channel.send(text)
             elif msg.content == '!념글크롤 정지':
                 if hot_post_crawler.run:
                     try:
                         hot_post_crawler.stop()
-                        await msg.author.send('성공적으로 정지 했습니다.')
+                        await msg.channel.send('성공적으로 정지 했습니다.')
                     except Exception as e:
                         text = '정지중 에러 발생 ' + str(e)
-                        await msg.author.send(text)
+                        await msg.channel.send(text)
                 else:
-                    await msg.author.send('이미 정지 되었습니다.')
+                    await msg.channel.send('이미 정지 되었습니다.')
             elif msg.content == '!념글크롤 상태':
                 text = '념글 크롤은 '
                 if hot_post_crawler.run:
                     text += '동작중'
                 else:
                     text += '정지중'
-                await msg.author.send(text + '입니다.')
+                await msg.channel.send(text + '입니다.')
 
 
 client.run(os.getenv('COVIDIC_BOT_TOKEN'))
